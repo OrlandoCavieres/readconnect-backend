73e8bdbdeae2c660befff6ef61eac2f6b22d08f0
Orlando Cavieres <frizortake@gmail.com>
2023-10-26 10:08:44 -0300

    Remove initial try query.

--- ReadConnect/main_schema.py
+++ ReadConnect/main_schema.py
@@ -1,25 +1,13 @@
-from strawberry import Schema, type, field
+from strawberry import Schema
 from strawberry.tools import merge_types
 
 from Books.schema import BookQueries, BookMutations
 from Interactions.schema import InteractionsQueries, InteractionsMutations
-from Schema.Permissions import IsAuthenticated
 from Users.schema import UsersMutations, UsersQueries
 
 
-@type
-class Query:
-    @field()
-    def hola(self) -> str:
-        return 'Hola mundo'
-
-    @field(permission_classes = [IsAuthenticated])
-    def prohibido(self) -> str:
-        return 'Prohibido'
-
-
 AllMutations = merge_types('AllMutations', (UsersMutations, BookMutations, InteractionsMutations))
-AllQueries = merge_types('AllQueries', (Query, BookQueries, InteractionsQueries, UsersQueries))
+AllQueries = merge_types('AllQueries', (BookQueries, InteractionsQueries, UsersQueries))
 
 
 schema = Schema(query = AllQueries, mutation = AllMutations)
